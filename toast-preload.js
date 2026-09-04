1236746a9babc489663a1aacbe9e8d065f8aee2d
Nicodemus Gurning <135511027+Volzeur@users.noreply.github.com>
2026-09-04 21:38:54 +0700

    Delete toast-preload.js

--- toast-preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('toast', {
-  confirm: () => ipcRenderer.send('toast:confirm'),
-  cancel: () => ipcRenderer.send('toast:cancel'),
-  onShow: (cb) => ipcRenderer.on('toast:show', (_e, data) => cb(data)),
-});
